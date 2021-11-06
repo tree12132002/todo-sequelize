dc42e7144763c45ca34371240d1b31d14d73b41c
yourname <tree12132002@hotmail.com>
2021-11-07 02:47:24 +0800

    add logout feature and add user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+const { authenticator } = require('../middleware/auth')
+
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
 
-router.use('/todos', todos)
+router.use('/todos', authenticator, todos)
 router.use('/users', users)
-router.use('/', home)
+router.use('/', authenticator, home)
 
 module.exports = router
