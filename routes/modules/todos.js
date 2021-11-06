097fa79d14613463e5f99a2ab113809e5297e1d6
yourname <tree12132002@hotmail.com>
2021-11-07 04:21:29 +0800

    add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,6 +4,20 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+// 登入新增頁面
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+// 
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+  return Todo.create({ name, UserId }) 
+    .then(() => res.redirect('/')) 
+    .catch(error => console.log(error))
+})
+
 // 登入詳情頁
 router.get('/:id', (req, res) => {
   const UserId = req.user.id
